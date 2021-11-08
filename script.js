648bbd714cb368c77a50ebfbbcddeb16b4524674
aileenunez <88999968+aileenunez@users.noreply.github.com>
2021-11-08 15:05:45 -0800

    added functions in JS to pull nested data

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-//save relevant API information 
+//Create Namespace 
+const basketApp = {};
 
+//save foundation URL in variable  
 basketApp.apiUrl = 'https://www.balldontlie.io/api/v1/stats'
 
 
 //function to request information from the API 
-
-basketApp.getStats = function(){
-
-
-    const url = new URL (basketApp.apiUrl)
+basketApp.getStats = function () {
+    const url = new URL(basketApp.apiUrl)
     url.search = new URLSearchParams({
-        page:8, 
+        start_date: '2021-10-18',
+        page: 1,
     })
     fetch(url)
-    .then(function(response){
-        return (response.json())
-        .then(function(data){
-            console.log(data)
+        .then(function (response) {
+            return (response.json())
+                .then(function (data) {
+                    basketApp.getPlayerInfo(data);
+                })
         })
-    })
-   
 }
 
-basketApp.getStats(); 
+//pull specific information from nested array 
+basketApp.getPlayerInfo = function (datafromApi) {
+    const playerObj = datafromApi.data;
+    playerObj.map(function (playerStats) {
+        console.log(playerStats)
+        // const { ast, blk } = playerStats
+        // console.log(ast, blk);
+        const { first_name, last_name } = playerStats.player
+        console.log(first_name, last_name);
+
+
+
+    });
+}
+
+
+//create init function in order for app to display JS once it is loaded
+basketApp.init = function () {
+    basketApp.getStats();
+}
+
+//Call the init method to kickstart the app
+basketApp.init()
